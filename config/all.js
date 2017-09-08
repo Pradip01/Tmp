3745ff5ee5493d852d4681956ca2c6b9746c11ea
Pradip_Kolhe <pradip.kolhe@raweng.com>
2017-09-08 10:43:53 +0530

    initial commit

---
+++ config/all.js
@@ -0,0 +1,19 @@
+module.exports = exports = {
+    port: "4000",
+    theme: "basic",
+    languages: [
+        {
+            "code": "en-us",
+            "relative_url_prefix": "/"
+        },
+        {
+            "code": "ja-jp",
+            "relative_url_prefix": "/jp"
+        }
+    ],
+    plugins: {},
+    contentstack: {
+        api_key: "bltd5320fc28cc2c0b4",
+        access_token: "blt2424b97c61f76fa0"
+    }
+};
