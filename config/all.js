64ca5112d7f9b3aa182ccdeedea240ac14b093ac
Pradip_Kolhe <pradip.kolhe@raweng.com>
2017-09-11 15:08:45 +0530

    test

--- config/all.js
+++ config/all.js
@@ -14,14 +14,24 @@ module.exports = exports = {
     plugins: {},
     storage: {
         provider: 'mongodb', // || adapter: 'mongodb'
-            dbName: 'heroku_1m62kr4r',
-            username: 'user',	// OPTIONAL
-            password: 'user1234',
-            server: {
-                host: 'ds129344.mlab.com',
-                port: 29344
-            }
+        dbName: 'heroku_1m62kr4r',
+        username: 'user',	// OPTIONAL
+        password: 'user1234',
+        server: {
+            host: 'ds129344.mlab.com',
+            port: 29344
+        }
     },
+   /* storage: {
+        provider: 'mongodb', // || adapter: 'mongodb'
+            dbName: 'new-cs',
+            // username: 'user',	// OPTIONAL
+            // password: 'user1234',
+            server: {
+                host: 'localhost',
+                port: 27017
+            }
+    },*/
     contentstack: {
         api_key: "bltd5320fc28cc2c0b4",
         access_token: "blt2424b97c61f76fa0"
